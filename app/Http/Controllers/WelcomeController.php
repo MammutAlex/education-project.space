24830de1a8fe4a61e585061de337685d45bb3faa
Alex <mammutcris@gmail.com>
2017-08-22 20:41:42 +0300

    Create about template

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -25,16 +25,24 @@ class WelcomeController extends Controller
     {
         return view('blog');
     }
+
     public function article()
     {
         return view('article');
     }
+
     public function contact()
     {
         return view('contact');
     }
+
     public function organizations()
     {
         return view('organizations');
+    }
+
+    public function about()
+    {
+        return view('about');
     }
 }
---
+++ resources/views/about.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+    <!--page header-->
+    <div class="page-breadcrumb grey-bg margin-b-60">
+        <div class="container">
+            <h4 class="margin-b-20">Наша команда</h4>
+        </div>
+    </div>
+    <div class="container margin-b-30">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 text-center margin-b-30">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum justo vitae convallis
+                    varius. Nulla tristique risus ut justo pulvinar mattis. Phasellus aliquet egestas mauris in
+                    venenatis. Nulla tristique risus ut justo pulvinar mattis. Phasellus aliquet egestas mauris in
+                    venenatis.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 margin-b-30">
+                <div class="person-box">
+                    <img src="images/team1.jpg" alt="" class="img-responsive margin-b-20">
+                    <h4>John Doe <br>
+                        <small>Ceo & Founder</small>
+                    </h4>
+                    <ul class="list-inline social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                    <p class="text-small">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.
+                    </p>
+                </div><!--/person-box-->
+            </div>
+            <div class="col-md-3 col-sm-6 margin-b-30">
+                <div class="person-box">
+                    <img src="images/team2.jpg" alt="" class="img-responsive margin-b-20">
+                    <h4>John Doe <br>
+                        <small>Ceo & Founder</small>
+                    </h4>
+                    <ul class="list-inline social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                    <p class="text-small">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.
+                    </p>
+                </div><!--/person-box-->
+            </div>
+            <div class="col-md-3 col-sm-6 margin-b-30">
+                <div class="person-box">
+                    <img src="images/team3.jpg" alt="" class="img-responsive margin-b-20">
+                    <h4>John Doe <br>
+                        <small>Ceo & Founder</small>
+                    </h4>
+                    <ul class="list-inline social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                    <p class="text-small">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.
+                    </p>
+                </div><!--/person-box-->
+            </div>
+            <div class="col-md-3 col-sm-6 margin-b-30">
+                <div class="person-box">
+                    <img src="images/team4.jpg" alt="" class="img-responsive margin-b-20">
+                    <h4>John Doe <br>
+                        <small>Ceo & Founder</small>
+                    </h4>
+                    <ul class="list-inline social-icons">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                    <p class="text-small">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.
+                    </p>
+                </div><!--/person-box-->
+            </div>
+        </div>
+    </div>
+@endsection
