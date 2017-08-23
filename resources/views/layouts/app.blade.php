7907a59c66cf54a27fa54f12360d250dea8e0c64
Alex <mammutcris@gmail.com>
2017-08-23 21:39:14 +0300

    Correct title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Common plugins -->
     <link href="/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
