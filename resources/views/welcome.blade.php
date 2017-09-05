101a10db71e9006a6f6c5fa076fe775bc0971ed7
Alex <mammutcris@gmail.com>
2017-09-05 11:25:21 +0300

    Correct welcome image width

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,17 +18,17 @@
         <div class="row">
             @foreach($directions as $index=>$direction)
                 @if((($index)%3) == 0 and $index != 0)
+        </div>
+        <div class="row">
+            @endif
+            <div class="col-sm-4 margin-b-30">
+                <a class="image-box" href="{{route('direction',['direction'=>$direction->url])}}">
+                    <img src="/{{$direction->photo}}" alt="{{$direction->title}}" class="img-responsive" width="550">
+                    <div class="img-overlay">
+                        <h1>{{$direction->title}}</h1>
                     </div>
-                    <div class="row">
-                @endif
-                <div class="col-sm-4 margin-b-30">
-                    <a class="image-box" href="{{route('direction',['direction'=>$direction->url])}}">
-                        <img src="/{{$direction->photo}}" alt="{{$direction->title}}" class="img-responsive">
-                        <div class="img-overlay">
-                            <h1>{{$direction->title}}</h1>
-                        </div>
-                    </a>
-                </div>
+                </a>
+            </div>
             @endforeach
         </div>
     </div>
